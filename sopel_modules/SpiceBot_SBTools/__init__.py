a91587c2bf02e77730b4bbb3abfeb3860a5a2d10
deathbybandaid <sam@deathbybandaid.net>
2019-05-04 17:21:48 -0400

    update \n logic

--- sopel_modules/SpiceBot_SBTools/__init__.py
+++ sopel_modules/SpiceBot_SBTools/__init__.py
@@ -95,12 +95,12 @@ def bot_logging(bot, logtype, logentry):
     if 'SpiceBot_Logs_queue' not in bot.memory:
         bot.memory['SpiceBot_Logs_queue'] = []
 
-    logmessage = "\n[" + logtype + "] " + logentry + "\n"
+    logmessage = "[" + logtype + "] " + logentry + ""
 
     if bot.config.SpiceBot_Logs.logging_channel:
         bot.memory['SpiceBot_Logs_queue'].append(logmessage)
 
-    stderr(logmessage)
+    stderr("\n" + logmessage + "\n")
 
     if 'SpiceBot_Logs' not in bot.memory:
         bot.memory['SpiceBot_Logs'] = {}
