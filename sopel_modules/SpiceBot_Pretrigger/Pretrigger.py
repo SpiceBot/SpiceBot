f7f0941b1a6ffd8b589a4e3ac9c7cdfe9f022bce
deathbybandaid <sam@deathbybandaid.net>
2019-05-05 16:28:23 -0400

    Update File

--- sopel_modules/SpiceBot_Pretrigger/Pretrigger.py
+++ sopel_modules/SpiceBot_Pretrigger/Pretrigger.py
@@ -4,32 +4,30 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 # sopel imports
 import sopel
-from sopel import module
-from sopel.tools import stderr
 
-from sopel import coretasks
-from sopel.trigger import PreTrigger, Trigger
-from sopel.bot import SopelWrapper
+from sopel.trigger import PreTrigger
+
 
 def setup(bot):
     sopel.tools._events.events.SpiceBotdbb = '7777'
 
 
-@module.event('7777')
-@module.rule('.*')
+@sopel.module.event('7777')
+@sopel.module.rule('.*')
 def parse_event_spicebotdbb(bot, trigger):
-    stderr("\n" + trigger.event + "    " + str(trigger.args) + "\n")
+    sopel.tools.stderr("\n" + trigger.event + "    " + str(trigger.args) + "\n")
 
 
-@module.event('001')
-@module.rule('.*')
+@sopel.module.event('001')
+@sopel.module.rule('.*')
 def bot_trigger_simulator(bot, trigger):
     bot_event_trigger_create(bot, 7777, "Welcome to the SpiceBot Test Event System!")
+
 
 def bot_event_trigger_create(bot, number, message):
 
     pretrigger = PreTrigger(
         bot.nick,
-        ":irc.spicebot.net " +  str(number) + " " + str(bot.nick) + " :" + message
+        ":SpiceBot_Event " + str(number) + " " + str(bot.nick) + " :" + message
     )
     bot.dispatch(pretrigger)
