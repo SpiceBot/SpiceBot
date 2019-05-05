1e349ae89c2c6c5ed5f65647bf5331da5a57f21b
deathbybandaid <sam@deathbybandaid.net>
2019-05-04 20:11:41 -0400

    Attempt to self trigger 001 RPL_Welcome

--- sopel_modules/SpiceBot_SBTools/__init__.py
+++ sopel_modules/SpiceBot_SBTools/__init__.py
@@ -253,7 +253,7 @@ def channel_list_current(bot):
 
 def spicebot_reload(bot):
     bot.reload_plugins()
-    bot.restart()
+    bot.write(('NOTICE', bot.nick), '001 Foo :Hello')
 
 
 def service_manip(bot, servicename, dowhat, log_from='service_manip'):
