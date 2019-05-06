c0ff28567a228222d5233b380010aca157497bbd
deathbybandaid <sam@deathbybandaid.net>
2019-05-06 15:26:50 -0400

    Invalid Command logic, disabled while migrating

---
+++ sopel_modules/SpiceBot_InvalidCommand/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf8
+"""Sopel Gif Search
+
+Sopel Gif Search builds a convenient framework for searching for Gifs
+"""
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+from .InvalidCommand import *
+
+__author__ = 'Sam Zick'
+__email__ = 'sam@deathbybandaid.net'
+__version__ = '0.1.1'
