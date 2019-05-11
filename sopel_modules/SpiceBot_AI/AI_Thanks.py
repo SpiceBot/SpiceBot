120dfec954a460aa5a01afa5809492cb0648b3c6
deathbybandaid <sam@deathbybandaid.net>
2019-05-11 16:39:07 -0400

    Split AI functionality

---
+++ sopel_modules/SpiceBot_AI/AI_Thanks.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+import sopel.module
+
+
+@sopel.module.rule(r'(?i)(thank you|thanks),? $nickname[ \t]*$')
+def bot_command_thanks(bot, trigger):
+    bot.reply("You're welcome.")
+
+
+@sopel.module.rule('$nickname thank you')
+def bot_command_thanks_b(bot, trigger):
+    bot_command_thanks(bot, trigger)
+
+
+@sopel.module.nickname_commands('thanks')
+def bot_command_thanks_c(bot, trigger):
+    bot_command_thanks(bot, trigger)
--- sopel_modules/SpiceBot_AI/__init__.py
+++ sopel_modules/SpiceBot_AI/__init__.py
@@ -2,6 +2,12 @@
 from __future__ import unicode_literals, absolute_import, division, print_function
 
 from .AI import *
+from .AI_Hello import *
+from .AI_Goodbye import *
+from .AI_Thanks import *
+from .AI_Acknowledge import *
+from .AI_Lazy import *
+from .AI_InsultHandler import *
 
 __author__ = 'Sam Zick'
 __email__ = 'sam@deathbybandaid.net'
