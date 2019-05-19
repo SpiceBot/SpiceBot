fcd34ff4f58bed1f524c43cccd5563d1b1be7a21
deathbybandaid <sam@deathbybandaid.net>
2019-05-18 20:37:46 -0400

    sleep remove

--- sopel_modules/SpiceBot_Events/Connected.py
+++ sopel_modules/SpiceBot_Events/Connected.py
@@ -6,14 +6,11 @@ import sopel.module
 
 from sopel_modules.SpiceBot.Events import botevents
 
-import time
-
 
 @sopel.module.event(botevents.BOT_WELCOME)
 @sopel.module.rule('.*')
 def bot_events_Connected(bot, trigger):
     """Here, we wait until we are in at least one channel"""
-    while not len(bot.channels.keys()):
+    while not len(bot.channels.keys()) > 0:
         pass
-    time.sleep(1)
     botevents.trigger(bot, botevents.BOT_CONNECTED, "Bot Connected to IRC")
