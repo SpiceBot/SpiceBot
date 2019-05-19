f63e1a7e1bd1a7b0712609785d4b2d1248d02cfd
deathbybandaid <sam@deathbybandaid.net>
2019-05-18 20:24:07 -0400

    events system should queue as soon as possible

--- sopel_modules/SpiceBot_Events/Queue.py
+++ sopel_modules/SpiceBot_Events/Queue.py
@@ -7,11 +7,10 @@ import sopel.module
 from sopel_modules.SpiceBot.Events import botevents
 
 
-@sopel.module.event(botevents.BOT_CONNECTED)
+@sopel.module.event(botevents.RPL_WELCOME)
 @sopel.module.rule('.*')
 def bot_events_connected(bot, trigger):
     """For items tossed in a queue, this will trigger them accordingly"""
-
     while True:
         if len(botevents.SpiceBot_Events["trigger_queue"]):
             pretriggerdict = botevents.SpiceBot_Events["trigger_queue"][0]
