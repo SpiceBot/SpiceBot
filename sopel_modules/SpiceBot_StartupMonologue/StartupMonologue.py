2cd3acd6c6d1a02ff7081c7b831dd8647631b9bf
deathbybandaid <sam@deathbybandaid.net>
2019-05-18 20:47:37 -0400

    tst

--- sopel_modules/SpiceBot_StartupMonologue/StartupMonologue.py
+++ sopel_modules/SpiceBot_StartupMonologue/StartupMonologue.py
@@ -10,7 +10,7 @@ from sopel_modules.SpiceBot.Events import botevents
 
 def setup(bot):
     botlogs.log('SpiceBot_StartupMonologue', "Starting setup procedure")
-    botevents.startup_add([botevents.BOT_STARTUPMONOLOGUE_CHANNELS, botevents.BOT_STARTUPMONOLOGUE_COMMANDSQUERY, botevents.BOT_STARTUPMONOLOGUE_CONNECTED])
+    botevents.startup_add([botevents.BOT_STARTUPMONOLOGUE_CHANNELS, botevents.BOT_STARTUPMONOLOGUE_COMMANDSQUERY])  # , botevents.BOT_STARTUPMONOLOGUE_CONNECTED])
 
     if 'SpiceBot_StartupMonologue' not in bot.memory:
         bot.memory['SpiceBot_StartupMonologue'] = []
