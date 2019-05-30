bf3b6e078c40b147784c32afc5df46f743d589f4
deathbybandaid <sam@deathbybandaid.net>
2019-05-30 15:37:22 -0400

    docstring

--- sopel_modules/SpiceBot/setup.py
+++ sopel_modules/SpiceBot/setup.py
@@ -1,6 +1,6 @@
 # coding=utf8
 from __future__ import unicode_literals, absolute_import, division, print_function
-"""Configuration for SpiceBot"""
+"""Setup for SpiceBot"""
 
 from .Logs import logs
 
