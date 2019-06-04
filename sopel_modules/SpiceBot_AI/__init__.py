28f901541101b06f8e994575afb5a426673664a4
deathbybandaid <sam@deathbybandaid.net>
2019-06-04 11:25:17 -0400

    goodbye

--- sopel_modules/SpiceBot_AI/__init__.py
+++ sopel_modules/SpiceBot_AI/__init__.py
@@ -6,14 +6,11 @@ from .AI_aiml import *
 from .AI_Hello import *
 from .AI_Goodbye import *
 from .AI_Thanks import *
-#from .AI_Acknowledge import *
 from .AI_Lazy import *
 from .AI_InsultHandler import *
-from .AI_Ping import *
 from .AI_Love import *
 from .AI_Really import *
 from .AI_Welcomeback import *
-# from .AI_Gender import *
 
 __author__ = 'Sam Zick'
 __email__ = 'sam@deathbybandaid.net'
