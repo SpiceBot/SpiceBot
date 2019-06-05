37deaadc8fda3c419fd92aa6a658fc05c7754dbf
deathbybandaid <sam@deathbybandaid.net>
2019-06-05 14:51:59 -0400

    more AI reworking

--- sopel_modules/SpiceBot_AI/__init__.py
+++ sopel_modules/SpiceBot_AI/__init__.py
@@ -3,13 +3,6 @@ from __future__ import unicode_literals, absolute_import, division, print_functi
 
 from .AI import *
 from .AI_aiml import *
-from .AI_Hello import *
-from .AI_Thanks import *
-from .AI_Lazy import *
-from .AI_InsultHandler import *
-from .AI_Love import *
-from .AI_Really import *
-from .AI_Welcomeback import *
 
 __author__ = 'Sam Zick'
 __email__ = 'sam@deathbybandaid.net'
