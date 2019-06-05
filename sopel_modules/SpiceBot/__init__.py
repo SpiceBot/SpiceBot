ecf1001a0b1b3c145ad29b7796ff5870899c85c0
deathbybandaid <sam@deathbybandaid.net>
2019-06-05 10:56:37 -0400

    reorder

--- sopel_modules/SpiceBot/__init__.py
+++ sopel_modules/SpiceBot/__init__.py
@@ -6,19 +6,20 @@ These are the core SpiceBot Classes
 This module contains references only for other modules to utilize.
 """
 
+from .Config import *
+from .Logs import *
+from .Database import *
+
 from .Update import *
 from .osd import *
 from .Kick import *
 
-from .Config import *
 from .Tools import *
 from .Prerun import *
-from .Logs import *
 from .Commands import *
 from .Events import *
 from .StartupMonologue import *
 from .Channels import *
-from .Database import *
 from .AI import *
 
 
