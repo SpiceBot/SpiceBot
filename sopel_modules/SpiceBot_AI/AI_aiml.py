2b8a8ac309321f4b477c6d9b5348913642ff9787
deathbybandaid <sam@deathbybandaid.net>
2019-06-05 18:15:59 -0400

    test

--- sopel_modules/SpiceBot_AI/AI_aiml.py
+++ sopel_modules/SpiceBot_AI/AI_aiml.py
@@ -59,7 +59,7 @@ def bot_command_rule(bot, trigger):
         trigger_args, trigger_command = SpiceBot.prerun.trigger_args(message, 'module')
         fulltrigger = spicemanip.main(trigger_args, 0)
 
-    returnmessage = SpiceBot.botai.on_message(bot, trigger, message)
+    returnmessage = SpiceBot.botai.on_message(bot, trigger, message.encode('utf-8'))
     if returnmessage:
         bot.osd(str(returnmessage))
     else:
