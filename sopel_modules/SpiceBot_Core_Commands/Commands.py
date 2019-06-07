ef39f23f5dc28d17e846def456c1f63843d58ceb
deathbybandaid <sam@deathbybandaid.net>
2019-06-07 11:13:14 -0400

    fix

--- sopel_modules/SpiceBot_Core_Commands/Commands.py
+++ sopel_modules/SpiceBot_Core_Commands/Commands.py
@@ -12,10 +12,10 @@ import spicemanip
 import sopel_modules.SpiceBot as SpiceBot
 
 
-# @SpiceBot.events.check_ready([SpiceBot.events.BOT_COMMANDS])
+@SpiceBot.events.check_ready([SpiceBot.events.BOT_COMMANDS])
 @SpiceBot.prerun.prerun('nickname')
 @sopel.module.nickname_commands('commands', 'command')
-def nickname_comand_channels(bot, trigger):
+def nickname_comand_commands(bot, trigger):
 
     if not len(trigger.sb['args']):
         commandused = 'list'
