094042e04e9ff8eee0b817616323bfa7b8aa6dae
deathbybandaid <sam@deathbybandaid.net>
2019-06-07 20:22:32 -0400

    test

--- setup.py
+++ setup.py
@@ -23,28 +23,6 @@ with open('requirements.txt') as requirements_file:
 with open('dev-requirements.txt') as dev_requirements_file:
     dev_requirements = [req for req in dev_requirements_file.readlines()]
 
-pipcommand = "sudo pip3 install --upgrade"
-pipcommand += " --force-reinstall"
-pipcommand += " git+https://github.com/sopel-irc/sopel@master"
-
-print('Running {} '.format(pipcommand), file=sys.stderr)
-for line in os.popen(pipcommand).read().split('\n'):
-    print(line, file=sys.stderr)
-
-import sopel
-
-# Remove stock modules, if present
-main_sopel_dir = os.path.dirname(os.path.abspath(sopel.__file__))
-modules_dir = os.path.join(main_sopel_dir, 'modules')
-stockdir = os.path.join(modules_dir, "stock")
-if not os.path.exists(stockdir) or not os.path.isdir(stockdir):
-    os.system("sudo mkdir " + stockdir)
-for pathname in os.listdir(modules_dir):
-    path = os.path.join(modules_dir, pathname)
-    if (os.path.isfile(path) and pathname.endswith('.py') and not pathname.startswith('_')):
-        os.system("sudo mv " + path + " " + stockdir)
-
-
 setup(
     name='sopel_modules.SpiceBot',
     version='0.1.1',
