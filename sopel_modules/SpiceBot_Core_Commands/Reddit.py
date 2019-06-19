15fed55dbe534cb20a0aed4cf92590e0bfefcab8
deathbybandaid <sam@deathbybandaid.net>
2019-06-19 11:10:14 -0400

    revert

--- sopel_modules/SpiceBot_Core_Commands/Reddit.py
+++ sopel_modules/SpiceBot_Core_Commands/Reddit.py
@@ -18,11 +18,5 @@ import sopel_modules.SpiceBot as SpiceBot
           )
           """)
 def bot_command_reddit_user(bot, trigger):
-    bot.say(str(trigger.group(0)))
-    bot.say(str(trigger.group(1)))
-    bot.say(str(trigger.group(2)))
-    bot.say(str(trigger.group(3)))
-    bot.say(str(trigger.group(4)))
-    bot.say(str(trigger.group(5)))
     message = SpiceBot.reddit.prerun(trigger)
     bot.osd(str(message))
