cb0503e2c9104af3c6529d677d77c4768644e074
deathbybandaid <sam@deathbybandaid.net>
2019-06-19 09:52:42 -0400

    test

--- sopel_modules/SpiceBot/Reddit.py
+++ sopel_modules/SpiceBot/Reddit.py
@@ -4,11 +4,22 @@ from __future__ import unicode_literals, absolute_import, division, print_functi
 This is the SpiceBot Reddit system.
 """
 
+import spicemanip
+
 
 class BotReddit():
     """This Logs all channels known to the server"""
     def __init__(self):
         self.dict = {}
+
+    def prerun(self, trigger):
+        trigger_args = spicemanip.main(trigger.args[1], 'create')
+        trigger.sb = {
+                        "slashcomm": spicemanip.main(trigger_args, 1).lower()[:1],
+                        "command": spicemanip.main(trigger_args, 1).lower()[2:],
+                        "args": spicemanip.main(trigger_args, "2+", 'list')
+                        }
+        return trigger
 
 
 reddit = BotReddit()
--- sopel_modules/SpiceBot_Core_Commands/Reddit.py
+++ sopel_modules/SpiceBot_Core_Commands/Reddit.py
@@ -4,9 +4,7 @@ from __future__ import unicode_literals, absolute_import, division, print_functi
 
 import sopel.module
 
-import spicemanip
-
-# import sopel_modules.SpiceBot as SpiceBot
+import sopel_modules.SpiceBot as SpiceBot
 
 
 @sopel.module.rule(r"""(?:)u/
@@ -15,7 +13,7 @@ import spicemanip
           )
           """)
 def bot_command_reddit_user(bot, trigger):
-    trigger = reddit_prerun(trigger)
+    trigger = SpiceBot.reddit.reddit_prerun(trigger)
     bot.osd(str(trigger.sb))
 
 
@@ -25,15 +23,5 @@ def bot_command_reddit_user(bot, trigger):
           )
           """)
 def bot_command_reddit_subreddit(bot, trigger):
-    trigger = reddit_prerun(trigger)
+    trigger = SpiceBot.reddit.reddit_prerun(trigger)
     bot.osd(str(trigger.sb))
-
-
-def reddit_prerun(trigger):
-    trigger_args = spicemanip.main(trigger.args[1], 'create')
-    trigger.sb = {
-                    "slashcomm": spicemanip.main(trigger_args, 1).lower()[:1],
-                    "command": spicemanip.main(trigger_args, 1).lower()[2:],
-                    "args": spicemanip.main(trigger_args, "2+", 'list')
-                    }
-    return trigger
