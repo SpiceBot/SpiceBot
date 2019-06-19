81867f24ee41be20cf80f924b9e0c40f863ff58d
deathbybandaid <sam@deathbybandaid.net>
2019-06-19 09:17:26 -0400

    test

--- sopel_modules/SpiceBot_Core_Commands/Reddit.py
+++ sopel_modules/SpiceBot_Core_Commands/Reddit.py
@@ -7,21 +7,13 @@ import sopel.module
 # import sopel_modules.SpiceBot as SpiceBot
 
 
-@sopel.module.rule(r"""(?:)u/
-          (
-            (?:\\/ | [^/])+
-          )
-          """)
+@sopel.module.rule(r"""(?:)u/((?:\\/ | [^/])+)""")
 def bot_command_reddit_user(bot, trigger):
     bot.osd(str(trigger))
     bot.osd(str(trigger.args))
 
 
-@sopel.module.rule(r"""(?:)r/
-          (
-            (?:\\/ | [^/])+
-          )
-          """)
+@sopel.module.rule(r"""(?:)r/((?:\\/ | [^/])+)""")
 def bot_command_reddit_subreddit(bot, trigger):
     bot.osd(str(trigger))
     bot.osd(str(trigger.args))
