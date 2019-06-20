fb43749c3c0611669a382787327761be35cae67c
deathbybandaid <sam@deathbybandaid.net>
2019-06-20 14:57:04 -0400

    test

--- sopel_modules/SpiceBot/DictComs.py
+++ sopel_modules/SpiceBot/DictComs.py
@@ -16,6 +16,8 @@ import requests
 from fake_useragent import UserAgent
 import urllib
 
+import sys
+
 
 class SpiceBot_DictComs_MainSection(StaticSection):
     extra = ListAttribute('extra')
@@ -34,6 +36,7 @@ class BotJSONCommands():
                                 ]
 
         dir_to_scan = botread.get_config_dirs("SpiceBot_DictComs")
+        sys.stderr(str(dir_to_scan))
 
         self.valid_dictcom_dict = botread.json_to_dict(dir_to_scan, "Dictionary Commands", "SpiceBot_DictComs")
 
