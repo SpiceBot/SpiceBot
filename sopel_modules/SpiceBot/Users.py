5c468e93cd308b775b47a15ba444e744c09e63d5
deathbybandaid <sam@deathbybandaid.net>
2019-06-28 11:03:18 -0400

    bandaid test

--- sopel_modules/SpiceBot/Users.py
+++ sopel_modules/SpiceBot/Users.py
@@ -5,7 +5,10 @@ from __future__ import unicode_literals, absolute_import, division, print_functi
 from sopel.tools import Identifier
 
 from .Database import db as botdb
-from .Tools import is_number
+from .Tools import is_number, inlist, similar, array_arrangesort
+from .Channels import channels as botchannels
+
+import spicemanip
 
 # TODO timestamp for new .seen
 
@@ -45,9 +48,13 @@ class BotUsers():
             nick_id = self.whois_ident(nickinput)
             return nick_id
 
-    def whois_ident(self, nick):
+    def whois_ident(self, nick, usercreate=True):
         nick = Identifier(nick)
-        nick_id = botdb.db.get_nick_id(nick, create=True)
+        try:
+            nick_id = botdb.db.get_nick_id(nick, create=usercreate)
+        except Exception as e:
+            nick_id = e
+            nick_id = None
         return int(nick_id)
 
     def save_user_db(self):
@@ -84,8 +91,8 @@ class BotUsers():
                 # set current nick
                 self.dict["current"][nick_id]["nick"] = user
                 # add joined channel to nick list
-                if channel not in self.dict["current"][nick_id]["channels"]:
-                    self.dict["current"][nick_id]["channels"].append(channel)
+                if str(channel).lower() not in self.dict["current"][nick_id]["channels"]:
+                    self.dict["current"][nick_id]["channels"].append(str(channel).lower())
                 # mark user as online
                 if nick_id not in self.dict["online"]:
                     self.dict["online"].append(int(nick_id))
@@ -104,8 +111,8 @@ class BotUsers():
                 # set current nick
                 self.dict["current"][nick_id]["nick"] = user
                 # add joined channel to nick list
-                if trigger.sender not in self.dict["current"][nick_id]["channels"]:
-                    self.dict["current"][nick_id]["channels"].append(trigger.sender)
+                if str(trigger.sender).lower() not in self.dict["current"][nick_id]["channels"]:
+                    self.dict["current"][nick_id]["channels"].append(str(trigger.sender).lower())
                 # mark user as online
                 if nick_id not in self.dict["online"]:
                     self.dict["online"].append(int(nick_id))
@@ -121,8 +128,8 @@ class BotUsers():
         # set current nick
         self.dict["current"][nick_id]["nick"] = trigger.nick
         # add joined channel to nick list
-        if trigger.sender not in self.dict["current"][nick_id]["channels"]:
-            self.dict["current"][nick_id]["channels"].append(trigger.sender)
+        if str(trigger.sender).lower() not in self.dict["current"][nick_id]["channels"]:
+            self.dict["current"][nick_id]["channels"].append(str(trigger.sender).lower())
         # mark user as online
         if nick_id not in self.dict["online"]:
             self.dict["online"].append(int(nick_id))
@@ -139,8 +146,8 @@ class BotUsers():
         # Verify nick is in the all list
         self.add_to_current(trigger.nick, nick_id)
         # empty nicks channel list
-        if trigger.sender in self.dict["current"][nick_id]["channels"]:
-            self.dict["current"][nick_id]["channels"].remove(trigger.sender)
+        if str(trigger.sender).lower() in self.dict["current"][nick_id]["channels"]:
+            self.dict["current"][nick_id]["channels"].remove(str(trigger.sender).lower())
         self.dict["current"][nick_id]["channels"] = []
         # mark user as offline
         if nick_id in self.dict["online"]:
@@ -151,8 +158,8 @@ class BotUsers():
     def part(self, bot, trigger):
         if trigger.nick == bot.nick:
             for nick_id in list(self.dict["current"].keys()):
-                if trigger.sender in self.dict["current"][nick_id]["channels"]:
-                    self.dict["current"][nick_id]["channels"].remove(trigger.sender)
+                if str(trigger.sender).lower() in self.dict["current"][nick_id]["channels"]:
+                    self.dict["current"][nick_id]["channels"].remove(str(trigger.sender).lower())
                 # mark offline
                 if not len(self.dict["current"][nick_id]["channels"]) and nick_id in self.dict["online"]:
                     self.dict["online"].remove(nick_id)
@@ -166,8 +173,8 @@ class BotUsers():
         # Verify nick is in the all list
         self.add_to_current(trigger.nick, nick_id)
         # remove channel from nick list
-        if trigger.sender in self.dict["current"][nick_id]["channels"]:
-            self.dict["current"][nick_id]["channels"].remove(trigger.sender)
+        if str(trigger.sender).lower() in self.dict["current"][nick_id]["channels"]:
+            self.dict["current"][nick_id]["channels"].remove(str(trigger.sender).lower())
         # mark offline
         if not len(self.dict["current"][nick_id]["channels"]) and nick_id in self.dict["online"]:
             self.dict["online"].remove(nick_id)
@@ -178,8 +185,8 @@ class BotUsers():
         targetnick = Identifier(str(trigger.args[1]))
         if targetnick == bot.nick:
             for nick_id in list(self.dict["current"].keys()):
-                if trigger.sender in self.dict["current"][nick_id]["channels"]:
-                    self.dict["current"][nick_id]["channels"].remove(trigger.sender)
+                if str(trigger.sender).lower() in self.dict["current"][nick_id]["channels"]:
+                    self.dict["current"][nick_id]["channels"].remove(str(trigger.sender).lower())
                 # mark offline
                 if not len(self.dict["current"][nick_id]["channels"]) and nick_id in self.dict["online"]:
                     self.dict["online"].remove(nick_id)
@@ -193,8 +200,8 @@ class BotUsers():
         # Verify nick is in the all list
         self.add_to_current(targetnick, nick_id)
         # remove channel from nick list
-        if trigger.sender in self.dict["current"][nick_id]["channels"]:
-            self.dict["current"][nick_id]["channels"].remove(trigger.sender)
+        if str(trigger.sender).lower() in self.dict["current"][nick_id]["channels"]:
+            self.dict["current"][nick_id]["channels"].remove(str(trigger.sender).lower())
         # mark offline
         if not len(self.dict["current"][nick_id]["channels"]) and nick_id in self.dict["online"]:
             self.dict["online"].remove(nick_id)
@@ -217,8 +224,8 @@ class BotUsers():
         # set current nick
         self.dict["current"][nick_id]["nick"] = newnick
         # add joined channel to nick list
-        if trigger.sender not in self.dict["current"][nick_id]["channels"]:
-            self.dict["current"][nick_id]["channels"].append(trigger.sender)
+        if str(trigger.sender).lower() not in self.dict["current"][nick_id]["channels"]:
+            self.dict["current"][nick_id]["channels"].append(str(trigger.sender).lower())
         # mark user as online
         if nick_id not in self.dict["online"]:
             self.dict["online"].append(int(nick_id))
@@ -228,12 +235,104 @@ class BotUsers():
     def mode(self, bot, trigger):
         return
 
-    def targetcheck(self):
+    def targetcheck(self, posstarget):
         return True
 
         # idk whois
         if not botdb.check_nick_id(posstarget):
             return
+
+    def nick_actual(self, nick, altlist=None):
+        nick_id = self.whois_ident(nick)
+        nick_actual = self.ID(nick_id)
+        return nick_actual
+
+    def target_online(self, nick, nick_id=None):
+        if not nick_id:
+            nick_id = self.ID(nick)
+        if nick_id in self.dict["online"]:
+            return True
+        else:
+            return False
+
+    def target_check(self, bot, trigger, target, targetbypass):
+        targetgood = {"targetgood": True, "error": "None", "reason": None}
+
+        if not isinstance(targetbypass, list):
+            targetbypass = [targetbypass]
+
+        if "notarget" not in targetbypass:
+            if not target or target == '':
+                return {"targetgood": False, "error": "No target Given.", "reason": "notarget"}
+
+        # Optional don't allow self-target
+        if "self" not in targetbypass:
+            if inlist(target, trigger.nick):
+                return {"targetgood": False, "error": "This command does not allow you to target yourself.", "reason": "self"}
+
+        # cannot target bots
+        if "bot" not in targetbypass:
+            if inlist(target, bot.nick):
+                return {"targetgood": False, "error": "I am a bot and cannot be targeted.", "reason": "bot"}
+        if "bots" not in targetbypass:
+            if inlist(target, bot.nick):
+                return {"targetgood": False, "error": self.nick_actual(bot, target) + " is a bot and cannot be targeted.", "reason": "bots"}
+
+        nick_id = self.whois_ident(target, usercreate=False)
+
+        # Not a valid user
+        if "unknown" not in targetbypass:
+            if not nick_id:
+                sim_user, sim_num = [], []
+                for nick_id in list(self.dict["all"].keys()):
+                    nick_list = self.dict["all"][nick_id]
+                    for nick in nick_list:
+                        similarlevel = similar(str(target).lower(), nick.lower())
+                        if similarlevel >= .75:
+                            sim_user.append(nick)
+                            sim_num.append(similarlevel)
+                if sim_user != [] and sim_num != []:
+                    sim_num, sim_user = array_arrangesort(bot, sim_num, sim_user)
+                    closestmatch = spicemanip.main(sim_user, 'reverse', "list")
+                    listnumb, relist = 1, []
+                    for item in closestmatch:
+                        if listnumb <= 3:
+                            relist.append(str(item))
+                        listnumb += 1
+                    closestmatches = spicemanip.main(relist, "andlist")
+                    targetgooderror = "It looks like you're trying to target someone! Did you mean: " + str(closestmatches) + "?"
+                else:
+                    targetgooderror = "I am not sure who that is."
+                return {"targetgood": False, "error": targetgooderror, "reason": "unknown"}
+
+        # User offline
+        if "offline" not in targetbypass:
+            if not self.target_online(target, nick_id):
+                return {"targetgood": False, "error": "It looks like " + self.nick_actual(bot, target) + " is offline right now!", "reason": "offline"}
+
+        # Private Message
+        if "privmsg" not in targetbypass:
+            if trigger.is_privmsg and not inlist(target, trigger.nick):
+                return {"targetgood": False, "error": "Leave " + self.nick_actual(bot, target) + " out of this private conversation!", "reason": "privmsg"}
+
+        # not in the same channel
+        if "diffchannel" not in targetbypass:
+            if not trigger.is_privmsg and self.target_online(target, nick_id):
+                if str(trigger.sender).lower() not in self.dict["current"][nick_id]["channels"]:
+                    return {"targetgood": False, "error": "It looks like " + self.nick_actual(bot, target) + " is online right now, but in a different channel.", "reason": "diffchannel"}
+
+        return targetgood
+
+    def random_valid_target(self, bot, trigger, outputtype):
+        validtargs = []
+        if trigger.is_privmsg:
+            validtargs.extend([str(bot.nick), trigger.nick])
+        else:
+            validtargs.extend(botchannels.dict["list"][str(trigger.sender).lower()]['users'])
+        if outputtype == 'list':
+            return validtargs
+        elif outputtype == 'random':
+            return spicemanip.main(validtargs, 'random')
 
 
 users = BotUsers()
