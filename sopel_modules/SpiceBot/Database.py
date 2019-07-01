320dde825567a897a8cca34b3ff4fcbb6cc019d4
deathbybandaid <sam@deathbybandaid.net>
2019-07-01 16:07:19 -0400

    test text

--- sopel_modules/SpiceBot/Database.py
+++ sopel_modules/SpiceBot/Database.py
@@ -50,7 +50,7 @@ class NickValues(BASE):
     nick_id = Column(Integer, ForeignKey('spice_nick_ids.nick_id'), primary_key=True)
     namespace = Column(String(255), primary_key=True)
     key = Column(String(255), primary_key=True)
-    value = Column(Text())
+    value = Column(Text(convert_unicode=True))
 
 
 class ChannelValues(BASE):
@@ -61,7 +61,7 @@ class ChannelValues(BASE):
     channel = Column(String(255), primary_key=True)
     namespace = Column(String(255), primary_key=True)
     key = Column(String(255), primary_key=True)
-    value = Column(Text())
+    value = Column(Text(convert_unicode=True))
 
 
 class PluginValues(BASE):
@@ -72,7 +72,7 @@ class PluginValues(BASE):
     plugin = Column(String(255), primary_key=True)
     namespace = Column(String(255), primary_key=True)
     key = Column(String(255), primary_key=True)
-    value = Column(Text())
+    value = Column(Text(convert_unicode=True))
 
 
 class SpiceDB(object):
