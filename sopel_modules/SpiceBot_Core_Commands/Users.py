1ead336e9b628ac45e231a5d557aabfed14b402d
deathbybandaid <sam@deathbybandaid.net>
2019-07-01 12:04:57 -0400

    user command

---
+++ sopel_modules/SpiceBot_Core_Commands/Users.py
@@ -0,0 +1,15 @@
+# coding=utf8
+from __future__ import unicode_literals, absolute_import, division, print_function
+"""
+This is the SpiceBot Users system.
+"""
+import sopel
+
+import sopel_modules.SpiceBot as SpiceBot
+
+
+@SpiceBot.prerun('nickname')
+@sopel.module.nickname_commands('users', 'user')
+def nickname_comand_channels(bot, trigger, botcom):
+    bot.say("WIP")
+    return
