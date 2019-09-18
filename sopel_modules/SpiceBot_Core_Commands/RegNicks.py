251f214ddc1e8c2aed4136dea6466321638dbc6e
deathbybandaid <sam@deathbybandaid.net>
2019-09-18 09:07:44 -0400

    nick not sender

--- sopel_modules/SpiceBot_Core_Commands/RegNicks.py
+++ sopel_modules/SpiceBot_Core_Commands/RegNicks.py
@@ -13,11 +13,11 @@ from sopel_modules.spicemanip import spicemanip
 @sopel.module.nickname_commands('regnicks', 'regnick')
 def nickname_comand_regnicks(bot, trigger, botcom):
     reggedusers = SpiceBot.users.dict["registered"]
-    bot.osd("These users are registered " + spicemanip(reggedusers, 'andlist') + ".", trigger.sender, 'notice')
+    bot.osd("These users are registered " + spicemanip(reggedusers, 'andlist') + ".", trigger.nick, 'notice')
     notregged = []
     for user in SpiceBot.users.dict["online"]:
         usernick = SpiceBot.users.ID(user)
         if str(usernick).lower() not in [x.lower() for x in SpiceBot.users.dict["registered"]]:
             if usernick != bot.nick:
                 notregged.append(usernick)
-    bot.osd("These online users are not registered " + spicemanip(notregged, 'andlist') + ".", trigger.sender, 'notice')
+    bot.osd("These online users are not registered " + spicemanip(notregged, 'andlist') + ".", trigger.nick, 'notice')
